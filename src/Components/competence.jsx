cdfd53384f3d58131d99a28076ae8f0a02ce808e
manel <manel.jouinipro@outlook.com>
2023-12-16 14:31:14 +0100

    VF

--- src/Components/competence.jsx
+++ src/Components/competence.jsx
@@ -28,11 +28,6 @@ function Competence(){
           <img src={Opti} alt='competences SEO'></img>
           </div>
         </div>
-       <div className='text-competence'>
-        <p>De la conception à la mise en ligne, je façonne des sites web complets avec passion et précision!  
-          Mon objectif est de donner vie à vos idées en créant des sites web fonctionnels, 
-          esthétiques et conviviaux, tout en offrant une expérience utilisateur exceptionnelle.</p>
-       </div>
       </div>
 
         </div>
